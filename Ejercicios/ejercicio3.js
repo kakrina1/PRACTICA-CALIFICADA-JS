21add43f900a3bdcd41d3be70981a9380b4b3eee
DonSeja06 <juanc124917@gmail.com>
2024-07-16 22:05:33 -0500

    Hice el ejercicio 3

--- Ejercicios/ejercicio3.js
+++ Ejercicios/ejercicio3.js
@@ -1,1 +1,15 @@
-console.log("prueba");
+let a=parseInt(prompt("Ingrese el primer número"));
+let b=parseInt(prompt("Ingrese el segundo número"));
+let c=prompt("Ingrese un operador (+,-,*,/)");
+
+if(c==="+"){
+    console.log(a+b);
+}else if(c==="-"){
+    console.log(a-b);
+}else if(c==="*"){
+    console.log(a*b);
+}else if(c==="/"){
+    console.log(a/b);
+}else{
+    console.log("Operador no valido");
+}
