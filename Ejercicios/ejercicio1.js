4e8d0c498b24dc2a726ea082176ce74f6bebe758
dchingap <dchingap@gmail.com>
2024-07-18 21:59:38 -0500

    ejercicio1 terminado

--- Ejercicios/ejercicio1.js
+++ Ejercicios/ejercicio1.js
@@ -1,1 +1,12 @@
-console.log("prueba");
+let a = 10;
+let b= 2;
+let c= 6;
+
+if (a >b && a > c) {    
+    console.log("La variable mayor es a = " +a);
+} else if (b > a && b > c) {
+    console.log("La variable mayor es b = " +b);
+} else {
+    c > a && c > b 
+    console.log("La variable mayor es C = " +c);
+}
