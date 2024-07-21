b9034ad2eb0b7f9a595608b961199d2b9a0a7e7c
Antony-2303 <antonyjose2303@gmail.com>
2024-07-21 11:52:16 -0500

    resolucion_2

---
+++ Ejercicios/ejercicio2.js
@@ -0,0 +1,19 @@
+let numero = 42;
+if (numero >= 0  && numero <= 10){
+    console.log("el numero esta entre 0 y 10");
+}
+else if (numero > 10 && numero <= 20) {
+    console.log("el numero esta entre 11 y 20");
+}
+else if (numero > 20 && numero <=30){
+    console.log("el numero esta entre 21 y 30");
+}
+else if (numero > 30 && numero <= 40){
+    console.log("el numero esta entre 31 y40");
+}
+else if (numero > 40 && numero <= 50){
+    console.log("el numero esta entre 41 y 50");
+}
+else {
+    console.log("el numero esta fuera del rango especifico");
+}
